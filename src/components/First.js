634042605804804b7b20a885f8a2396020675a8d
Anamika Sawhney <anamika.sawhney22@gmail.com>
2024-09-06 10:05:08 +0530

    react bootstrap

---
+++ src/components/First.js
@@ -0,0 +1,34 @@
+import React from 'react'
+// import './First.css';
+export const First = () => {
+  return (
+    <> 
+  <h1> First Component </h1>
+  <h1> First Component </h1>
+    <div> 100 </div>
+    <div> 200 </div>
+    <div> 300 </div>
+    <h1 id="id1"> About CSS </h1>
+    <p className='class1'> para 1</p>
+    <p> para 2 </p>
+    <h2 className='class1'> heading 2 </h2>
+    <p className='martel-sans-bold '>  
+    A text can be left or right aligned, centered, or justified.
+
+The following example shows center aligned, and left and right aligned text (left alignment is default if text direction is left-to-right, and right alignment is default if text direction is right-to-left):A text can be left or right aligned, centered, or justified.
+
+The following example shows center aligned, and left and right aligned text (left alignment is default if text direction is left-to-right, and right alignment is default if text direction is right-to-left):A text can be left or right aligned, centered, or justified.
+
+The following example shows center aligned, and left and right aligned text (left alignment is default if text direction is left-to-right, and right alignment is default if text direction is right-to-left):A text can be left or right aligned, centered, or justified.
+
+The following example shows center aligned, and left and right aligned text (left alignment is default if text direction is left-to-right, and right alignment is default if text direction is right-to-left):A text can be left or right aligned, centered, or justified.
+
+The following example shows center aligned, and left and right aligned text (left alignment is default if text direction is left-to-right, and right alignment is default if text direction is right-to-left):A text can be left or right aligned, centered, or justified.
+
+The following example shows center aligned, and left and right aligned text (left alignment is default if text direction is left-to-right, and right alignment is default if text direction is right-to-left):A text can be left or right aligned, centered, or justified.
+
+The following example shows center aligned, and left and right aligned text (left alignment is default if text direction is left-to-right, and right alignment is default if text direction is right-to-left):
+    </p>
+    </>
+  )
+}
